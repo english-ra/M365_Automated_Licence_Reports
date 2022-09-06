7865d246ea788987d218c0304b532235e67d25ec
english-ra <63851638+english-ra@users.noreply.github.com>
2022-09-06 23:52:49 +0100

    Update M365_Licence_Report.ps1

--- M365_Licence_Report.ps1
+++ M365_Licence_Report.ps1
@@ -3,12 +3,15 @@ Import-Module Microsoft.Graph.Users.Actions
 $clientID = ""
 $tenantId = ""
 $certificateName = ""
+
+$licenceDetailsCSVPath = ""
+$reportExportPath = ""
  
 # Authenticate with Graph API
 Connect-MgGraph -ClientID $clientID -TenantId $tenantId -CertificateName $certificateName
  
 # Import the licence details CSV
-$plansCSV = Import-Csv -Path "C:\source\scripts\M365 Reporting\licenceDetails.csv"
+$plansCSV = Import-Csv -Path $licenceDetailsCSVPath
  
 # Get the users
 $users = Get-MgUser -Filter 'assignedLicenses/$count ne 0' -ConsistencyLevel eventual -CountVariable licensedUserCount -All -Select UserPrincipalName,DisplayName,AssignedLicenses,JobTitle,Department
@@ -20,12 +23,17 @@ $customUsers = New-Object System.Collections.ArrayList
 # Get the licence names
 foreach ($user in $users) {
     $licenceNames = ""
+    $licenceCount = 0
  
     # Map the licence SKU to the licence's name
     foreach ($licence in $user.AssignedLicenses) {
         $licenceDetails = $plansCSV | Where-Object { $_.GUID -eq $licence.skuId } | Select-Object -Unique
         $licenceNames += $licenceDetails.Product_Display_Name + ", "
+        $licenceCount += 1
     }
+
+    # Add the licence count to the end
+    $licenceNames += "(" + $licenceCount + ")"
  
     # Create a custom user object
     $customUser = @{
@@ -45,16 +53,18 @@ foreach ($user in $users) {
 $customUsers | Format-Table -Property UPN,DisplayName,JobTitle,Department,Licences
 
 # Create a csv of all the licenced users
-$reportCSV = $customUsers | Export-Csv -Path "C:\source\scripts\M365 Reporting\Exports\report.csv"
- 
- 
+$customUsers | select DisplayName, UPN, JobTitle, Department, licences | Export-Csv -Path $reportExportPath -NoTypeInformation
+
+# Convert the csv file to base64
+$base64string = [Convert]::ToBase64String([IO.File]::ReadAllBytes($reportExportPath))
+
 # Send the email
 $params = @{
     Message = @{
         Subject = "M365 Licence Report"
         Body = @{
             ContentType = "Text"
-            Content = "Hi, I hope that you're well. Please find attached a report of all current users and the licences that they have assigned. Kind regards,"
+            Content = "Hi,`r`rI hope that you're well.`r`rPlease find attached a report of all current users and the licences that they have assigned.`r`rKind regards,`r`r"
         }
         ToRecipients = @(
             @{
@@ -70,6 +80,14 @@ $params = @{
                 }
             }
         )
+        Attachments = @(
+			@{
+				"@odata.type" = "#microsoft.graph.fileAttachment"
+				Name = "usersReport.csv"
+				ContentType = "text/plain"
+				ContentBytes = $base64string
+			}
+		)
     }
     SaveToSentItems = "false"
 }

dd9a1d506b719afca30f58c0a46819ce9e5b86b7
english-ra <63851638+english-ra@users.noreply.github.com>
2022-09-07 17:52:11 +0100

    Update M365_Licence_Report.ps1

--- M365_Licence_Report.ps1
+++ M365_Licence_Report.ps1
@@ -1,15 +1,15 @@
 Import-Module Microsoft.Graph.Users.Actions
-
-
+ 
+ 
 $clientName = ""
-
+ 
 $clientID = ""
 $tenantId = ""
 $certificateName = ""
-
+ 
 $licenceDetailsCSVPath = ""
 $reportExportPath = ""
-
+ 
  
 # Authenticate with Graph API
 Connect-MgGraph -ClientID $clientID -TenantId $tenantId -CertificateName $certificateName
@@ -21,7 +21,7 @@ $plansCSV = Import-Csv -Path $licenceDetailsCSVPath
 $users = Get-MgUser -Filter 'assignedLicenses/$count ne 0' -ConsistencyLevel eventual -CountVariable licensedUserCount -All -Select UserPrincipalName, GivenName, Surname, DisplayName, JobTitle, Department, AssignedLicenses
  
 Write-Output("The total amount of licenced users is " + $licensedUserCount)
-
+ 
 $customUsers = New-Object System.Collections.ArrayList
  
 # Get the licence names
@@ -32,10 +32,19 @@ foreach ($user in $users) {
     # Map the licence SKU to the licence's name
     foreach ($licence in $user.AssignedLicenses) {
         $licenceDetails = $plansCSV | Where-Object { $_.GUID -eq $licence.skuId } | Select-Object -Unique
-        $licenceNames += $licenceDetails.Product_Display_Name + ", "
+ 
+        #Display error if licence name can't be found
+        if ( $licenceDetails.Product_Display_Name -eq $null ) { $licenceNames += "[UNABLE TO LOCATE LICENCE NAME]" }
+        else { $licenceNames += $licenceDetails.Product_Display_Name }
+        $licenceNames += ", "
         $licenceCount += 1
+ 
+        Write-Output($licence.skuId)
+        Write-Output($licenceNames)
     }
-
+    Write-Output($user.DisplayName)
+    Write-Output('-')
+ 
     # Add the licence count to the end
     $licenceNames += "(" + $licenceCount + ")"
  
@@ -57,19 +66,19 @@ foreach ($user in $users) {
  
 # Display a table of all the licenced users
 $customUsers | Format-Table -Property UserPrincipalName, GivenName, Surname, DisplayName, JobTitle, Department, AssignedLicenses
-
+ 
 # Create a csv of all the licenced users
 $customUsers | Sort-Object -Property Surname | select UserPrincipalName, GivenName, Surname, DisplayName, JobTitle, Department, AssignedLicenses | Export-Csv -Path $reportExportPath -NoTypeInformation
-
+ 
 # Convert the csv file to base64
 $base64string = [Convert]::ToBase64String([IO.File]::ReadAllBytes($reportExportPath))
-
+ 
 # Send the email
-
+ 
 $date = Get-Date -Format "dd/MM/yyyy"
 $params = @{
     Message = @{
-        Subject = "M365 Licence Report"
+        Subject = "M365 Licence Report - " + $clientName + " - " + $date
         Body = @{
             ContentType = "Text"
             Content = "Hi,`r`rI hope that you're well.`r`rPlease find attached a report of all current users and the licences that they have assigned.`r`rKind regards,`r`r"
@@ -96,13 +105,13 @@ $params = @{
             }
         )
         Attachments = @(
-			@{
-				"@odata.type" = "#microsoft.graph.fileAttachment"
-				Name = "M365-Licences_" + $clientName + "_" + $date + ".csv"
-				ContentType = "text/plain"
-				ContentBytes = $base64string
-			}
-		)
+                                                @{
+                                                                "@odata.type" = "#microsoft.graph.fileAttachment"
+                                                                Name = "M365-Licences_" + $clientName + "_" + $date + ".csv"
+                                                                ContentType = "text/plain"
+                                                                ContentBytes = $base64string
+                                                }
+                                )
     }
     SaveToSentItems = "false"
 }
@@ -110,4 +119,4 @@ Send-MgUserMail -UserId "" -BodyParameter $params
  
  
 # Disconnect from Graph API
-Disconnect-MgGraph
+Disconnect-MgGraph 
